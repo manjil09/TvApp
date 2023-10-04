338dc2cc221d028345cd6af560ead0be1433be33
manjil <manjilkoju09@gmail.com>
2023-10-04 10:37:27 +0545

    added video description and other rows

--- app/src/main/java/com/manjil/tvapplication/playbackPage/VideoPlaybackFragment.kt
+++ app/src/main/java/com/manjil/tvapplication/playbackPage/VideoPlaybackFragment.kt
@@ -1,26 +1,74 @@
 package com.manjil.tvapplication.playbackPage
 
+import android.os.Build
 import androidx.leanback.app.PlaybackSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.ClassPresenterSelector
 import androidx.leanback.widget.ControlButtonPresenterSelector
+import androidx.leanback.widget.DetailsOverviewRowPresenter
+import androidx.leanback.widget.HeaderItem
+import androidx.leanback.widget.ListRow
+import androidx.leanback.widget.ListRowPresenter
 import androidx.leanback.widget.PlaybackControlsRow
 import androidx.leanback.widget.PlaybackControlsRowPresenter
+import com.manjil.tvapplication.CardPresenter
+import com.manjil.tvapplication.detailsPage.DetailsDescriptionPresenter
+import com.manjil.tvapplication.model.Movie
+import java.io.Serializable
 
-class VideoPlaybackFragment: PlaybackSupportFragment() {
-    private lateinit var controllerActionAdapter: ArrayObjectAdapter
+class VideoPlaybackFragment : PlaybackSupportFragment() {
+    private var selectedMovie: Movie? = null
     private lateinit var mAdapter: ArrayObjectAdapter
     override fun onStart() {
         super.onStart()
-        mAdapter = ArrayObjectAdapter(PlaybackControlsRowPresenter())
+        selectedMovie = getSerializable("movie")
 
+        setupRows()
         setupController()
+        addOtherRows()
 
+//        isControlsOverlayAutoHideEnabled = true
         adapter = mAdapter
     }
 
+    private fun addOtherRows() {
+        val listRowAdapter = ArrayObjectAdapter(CardPresenter())
+        listRowAdapter.add(
+            Movie(
+                "First Title",
+                "Description for first title",
+                "https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg"
+            )
+        )
+        listRowAdapter.add(
+            Movie(
+                "Second Title",
+                "Description for second title",
+                "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg"
+            )
+        )
+        listRowAdapter.add(
+            Movie(
+                "Third Title",
+                "Description for third title",
+                "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg"
+            )
+        )
+        mAdapter.add(ListRow(HeaderItem("More like this"),listRowAdapter))
+    }
+
+    private fun setupRows() {
+        val presenterSelector = ClassPresenterSelector()
+        val playbackControlsRowPresenter = PlaybackControlsRowPresenter(DetailsDescriptionPresenter())
+
+        presenterSelector.addClassPresenter(PlaybackControlsRow::class.java, playbackControlsRowPresenter)
+        presenterSelector.addClassPresenter(ListRow::class.java, ListRowPresenter())
+
+        mAdapter = ArrayObjectAdapter(presenterSelector)
+    }
     private fun setupController() {
-        controllerActionAdapter = ArrayObjectAdapter(ControlButtonPresenterSelector())
-        val playbackControlsRow = PlaybackControlsRow()
+        val controllerActionAdapter = ArrayObjectAdapter(ControlButtonPresenterSelector())
+        val playbackControlsRow = PlaybackControlsRow(selectedMovie)
         mAdapter.add(playbackControlsRow)
 
         playbackControlsRow.primaryActionsAdapter = controllerActionAdapter
@@ -40,4 +88,10 @@ class VideoPlaybackFragment: PlaybackSupportFragment() {
             add(skipNextAction)
         }
     }
+    @Suppress("DEPRECATION")
+    private inline fun <reified T : Serializable> getSerializable(key: String): T? =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+            requireActivity().intent.getSerializableExtra(key) as T
+        else
+            requireActivity().intent.getSerializableExtra(key, T::class.java)
 }
